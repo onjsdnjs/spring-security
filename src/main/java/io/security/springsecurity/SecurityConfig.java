710677d3d40c99d0e8109a24d5f0d671f874bd36
onjsdnjs <onjsdnjs@gmail.com>
2022-11-29 13:28:28 +0900

    lecture

--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -3,27 +3,22 @@ package io.security.springsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests()
+//                .antMatchers("/login**").permitAll()
+                .anyRequest().authenticated();
         http.formLogin()
-                .loginPage("/login")
+//                .loginPage("/login")
                 .defaultSuccessUrl("/home")
                 .failureUrl("/error")
                 .usernameParameter("userId")
@@ -31,9 +26,12 @@ public class SecurityConfig {
                 .loginProcessingUrl("/loginProc")
                 .successHandler((request, response, authentication) -> {
                     System.out.println("authentication: " + authentication);
+                    response.sendRedirect("/");
                 })
                 .failureHandler((request, response, exception) -> {
                     System.out.println("authentication: " + exception.getMessage());
+//                    response.sendRedirect("/login?error=true");
+                    response.sendRedirect("/login");
                 })
                 .permitAll();
 
@@ -50,6 +48,7 @@ public class SecurityConfig {
                 })
                 .logoutSuccessHandler((request, response, authentication) -> {
                     System.out.println("logout is succeed");
+                    response.sendRedirect("/login");
                 });
 
         return http.build();
