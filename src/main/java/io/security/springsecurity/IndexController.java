b537480c1cbb745c5f720247ee939f988b3e7578
onjsdnjs <onjsdnjs@gmail.com>
2022-11-29 17:21:05 +0900

    lecture

--- src/main/java/io/security/springsecurity/IndexController.java
+++ src/main/java/io/security/springsecurity/IndexController.java
@@ -11,4 +11,19 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/shop/mypage")
+    public String mypage(){
+        return "mypage";
+    }
+
+    @GetMapping("/shop/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/shop/admin/pay")
+    public String adminpay(){
+        return "adminpay";
+    }
+
 }
