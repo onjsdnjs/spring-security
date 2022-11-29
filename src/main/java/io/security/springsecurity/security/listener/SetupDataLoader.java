7c0814fdb4c10cba50f5adf6abfc7b7d31937ca0
onjsdnjs <onjsdnjs@gmail.com>
2022-11-30 00:52:11 +0900

    lecture

--- src/main/java/io/security/springsecurity/security/listener/SetupDataLoader.java
+++ src/main/java/io/security/springsecurity/security/listener/SetupDataLoader.java
@@ -24,7 +24,6 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     @Autowired
     private RoleRepository roleRepository;
 
-
     @Autowired
     private ResourcesRepository resourcesRepository;
 
