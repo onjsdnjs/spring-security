07b57f0cb3f1edd76cf17504397f49c578d268f9
onjsdnjs <onjsdnjs@gmail.com>
2022-11-29 22:03:52 +0900

    lecture

---
+++ src/main/java/io/security/springsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package io.security.springsecurity.service.impl;
+
+import io.security.springsecurity.domain.entity.Account;
+import io.security.springsecurity.repository.UserRepository;
+import io.security.springsecurity.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public void createUser(Account account){
+        userRepository.save(account);
+    }
+}
