e79724fd7ea49f2aa47578db4a0a7f7100c98dc7
onjsdnjs <onjsdnjs@gmail.com>
2022-11-29 19:10:25 +0900

    lecture

--- src/main/java/io/security/springsecurity/IndexController.java
+++ src/main/java/io/security/springsecurity/IndexController.java
@@ -1,13 +1,22 @@
 package io.security.springsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Authentication authentication, @AuthenticationPrincipal UserDetails user){
+
+        UserDetails principal = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("principal = " + principal);
+        System.out.println("authentication = " + authentication + ", user = " + user);
+
         return "index";
     }
 
--- src/main/java/io/security/springsecurity/LoginController.java
+++ src/main/java/io/security/springsecurity/LoginController.java
@@ -1,7 +1,12 @@
 package io.security.springsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -14,5 +19,15 @@ public class LoginController {
     @GetMapping("/denied")
     public String denied(){
         return "denied";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication ) {
+
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, response, authentication);
+
+        return "redirect:/login";
+
     }
 }
