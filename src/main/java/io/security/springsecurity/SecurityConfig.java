730b9e2b654d1248e586c66887f70aaad1e7f0e2
jeongsuwon <onjsdnjs@gmail.com>
2022-12-02 19:07:36 +0900

    lecture

--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -17,6 +18,9 @@ public class SecurityConfig {
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
                 ;
+//        http.securityContext(securityContext -> securityContext.requireExplicitSave(true));
+        http.securityContext(httpSecuritySecurityContextConfigurer ->
+                httpSecuritySecurityContextConfigurer.securityContextRepository(new RequestAttributeSecurityContextRepository()));
         http.formLogin();
         return http.build();
     }
