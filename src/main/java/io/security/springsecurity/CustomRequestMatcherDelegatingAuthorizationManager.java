3fbcdeaa44c20611bf239380c3ec3bbbc1368906
jeongsuwon <onjsdnjs@gmail.com>
2022-12-02 15:49:28 +0900

    lecture

--- src/main/java/io/security/springsecurity/CustomRequestMatcherDelegatingAuthorizationManager.java
+++ src/main/java/io/security/springsecurity/CustomRequestMatcherDelegatingAuthorizationManager.java
@@ -1,25 +1,17 @@
 package io.security.springsecurity;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.core.log.LogMessage;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcherEntry;
 import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.function.Supplier;
 
-public class CustomRequestMatcherDelegatingAuthorizationManager implements AuthorizationManager<HttpServletRequest> {
+public class CustomRequestMatcherDelegatingAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
     private static final AuthorizationDecision DENY = new AuthorizationDecision(false);
-
-    private final Log logger = LogFactory.getLog(getClass());
-
     private final List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings;
 
     public CustomRequestMatcherDelegatingAuthorizationManager(
@@ -27,27 +19,16 @@ public class CustomRequestMatcherDelegatingAuthorizationManager implements Autho
         Assert.notEmpty(mappings, "mappings cannot be empty");
         this.mappings = mappings;
     }
+    @Override
+    public void verify(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        AuthorizationManager.super.verify(authentication, object);
+    }
 
     @Override
-    public AuthorizationDecision check(Supplier<Authentication> authentication, HttpServletRequest request) {
-        if (this.logger.isTraceEnabled()) {
-            this.logger.trace(LogMessage.format("Authorizing %s", request));
-        }
+    public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
         for (RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> mapping : this.mappings) {
-
-            RequestMatcher matcher = mapping.getRequestMatcher();
-            RequestMatcher.MatchResult matchResult = matcher.matcher(request);
-            if (matchResult.isMatch()) {
                 AuthorizationManager<RequestAuthorizationContext> manager = mapping.getEntry();
-                if (this.logger.isTraceEnabled()) {
-                    this.logger.trace(LogMessage.format("Checking authorization on %s using %s", request, manager));
-                }
-                return manager.check(authentication,
-                        new RequestAuthorizationContext(request, matchResult.getVariables()));
-            }
-        }
-        if (this.logger.isTraceEnabled()) {
-            this.logger.trace(LogMessage.of(() -> "Denying request since did not find matching RequestMatcher"));
+                return manager.check(authentication,object);
         }
         return DENY;
     }
--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -1,46 +1,57 @@
 package io.security.springsecurity;
 
 import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.access.intercept.RequestMatcherDelegatingAuthorizationManager;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcherEntry;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().shouldFilterAllDispatcherTypes(false)
-                .dispatcherTypeMatchers(DispatcherType.ERROR)
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,AuthorizationManager<RequestAuthorizationContext> access) throws Exception {
+        http.authorizeHttpRequests().anyRequest().access(access)
 //                .requestMatchers("/user").hasRole("USER")
 //                .requestMatchers("/admin").hasRole("ADMIN")
-        .authenticated()
+//                .anyRequest().authenticated();
                 ;
         http.formLogin();
-        http.addFilterAt(customAuthorizationFilter(null), AuthorizationFilter.class);
+//        http.addFilterAt(customAuthorizationFilter(), AuthorizationFilter.class);
         return http.build();
     }
 
-    @Bean
-    public CustomAuthorizationFilter customAuthorizationFilter(ApplicationContext applicationContext){
+    /*@Bean
+    public CustomAuthorizationFilter customAuthorizationFilter(){
 
         HandlerMappingIntrospector mvcHandlerMappingIntrospector =
                 applicationContext.getBean("mvcHandlerMappingIntrospector", HandlerMappingIntrospector.class);
@@ -65,5 +76,49 @@ public class SecurityConfig {
         CustomRequestMatcherDelegatingAuthorizationManager authorizationManager = new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
 
         return new CustomAuthorizationFilter(authorizationManager);
+    }*/
+
+    /*@Bean
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(){
+
+        HandlerMappingIntrospector mvcHandlerMappingIntrospector = applicationContext.getBean("mvcHandlerMappingIntrospector", HandlerMappingIntrospector.class);
+
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings
+                = new ArrayList<>();
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1 = new RequestMatcherEntry<>(new MvcRequestMatcher(mvcHandlerMappingIntrospector, "/user")
+                , new CustomAuthorizationManager<>("ROLE_USER"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2 = new RequestMatcherEntry<>(new MvcRequestMatcher(mvcHandlerMappingIntrospector, "/admin")
+                , new CustomAuthorizationManager<>("ROLE_ADMIN"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3 = new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE
+                , new AuthenticatedAuthorizationManager<>());
+
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
+    }*/
+
+    @Bean
+    AuthorizationManager<RequestAuthorizationContext> requestMatcherAuthorizationManager(HandlerMappingIntrospector introspector) {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        RequestMatcher permitAll =
+                new AndRequestMatcher(
+                        mvcMatcherBuilder.pattern("/resources/**"),
+                        mvcMatcherBuilder.pattern("/signup"),
+                        mvcMatcherBuilder.pattern("/about"));
+        RequestMatcher admin = mvcMatcherBuilder.pattern("/admin/**");
+        RequestMatcher db = mvcMatcherBuilder.pattern("/db/**");
+        RequestMatcher any = AnyRequestMatcher.INSTANCE;
+        AuthorizationManager<HttpServletRequest> manager = RequestMatcherDelegatingAuthorizationManager.builder()
+                .add(permitAll, (authentication,object) -> new AuthorizationDecision(true))
+                .add(admin, AuthorityAuthorizationManager.hasRole("ADMIN"))
+                .add(db, AuthorityAuthorizationManager.hasRole("DBA"))
+                .add(any, new AuthenticatedAuthorizationManager<>())
+                .build();
+        return (authentication, context) -> manager.check(authentication,context.getRequest());
     }
 }
