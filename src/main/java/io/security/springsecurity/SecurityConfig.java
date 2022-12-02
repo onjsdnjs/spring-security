2b200b43826a1363fe0b2c9e8229312863917713
jeongsuwon <onjsdnjs@gmail.com>
2022-12-02 17:40:08 +0900

    lecture

--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -1,49 +1,21 @@
 package io.security.springsecurity;
 
-import jakarta.servlet.DispatcherType;
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-import org.springframework.security.web.access.intercept.RequestMatcherDelegatingAuthorizationManager;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcherEntry;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Supplier;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,AuthorizationManager<RequestAuthorizationContext> access) throws Exception {
-        http.authorizeHttpRequests().anyRequest().access(access)
-//                .requestMatchers("/user").hasRole("USER")
-//                .requestMatchers("/admin").hasRole("ADMIN")
-//                .anyRequest().authenticated();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/user").hasRole("USER")
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
                 ;
         http.formLogin();
         return http.build();
